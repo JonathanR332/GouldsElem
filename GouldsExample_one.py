004d92aab49549aa97b052bfef124685d1c8605c
Pink <130505226+JonathanR332@users.noreply.github.com>
2024-03-04 19:35:48 -0500

    GouldsExample_one.py
    Added comments

--- GouldsExample_one.py
+++ GouldsExample_one.py
@@ -1,33 +1,33 @@
 from os import close
 import random as ran 
 
-print(2*2)
+print(2*2) # printing common math statements that they would know
 print(2+2)
 print(3-2)
 print(3/2)
 print(3**3)
-print("Hello World")
+print("Hello World") # printing text  
 
 age = input("How old are you? ")
-name = input("What is your name? ")
+name = input("What is your name? ") #asking inputs for then to be displayed back 
 score = 0
 
 print("Hello, I am Mario" +"\nNice to meet you " + name + "\nIt is nice hearing you are " + age + " Years old. ")
 
-print(ran.randint(0,100000))
+print(ran.randint(0,100000)) #random number for fun
 
 
 def my_quiz():
   print("Welcome to my quiz")
   score = 0
 Answer = input("What Color is the sky? ")
-if Answer == "Blue" or Answer == "blue":
+if Answer == "Blue" or Answer == "blue": # intro to if statements however they could go avoiding it
   print("Correct")
-  score  += 1
+  score  += 1 # score going up; introduction to operations.
   print("Your score is " + str(score))
 else:
   print("Incorrect")
-  print("Your score is " + str(score)) 
+  print("Your score is " + str(score))  # display score and feedback
 
 Answer2 = input("What color is grass? ")
 if Answer2 == "Green" or Answer2 == "green":
@@ -48,4 +48,4 @@ else:
   print("Your score is " + str(score)) 
 
 
-print("The end: \n Thank you for playing " + name + " You got " + str(score) + " out of 3")
+print("The end: \n Thank you for playing " + name + " You got " + str(score) + " out of 3") # Final message to show users their score.
