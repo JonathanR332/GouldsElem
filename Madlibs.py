56546e03744a2920940b6026526c7f6f8d7006ab
Jonathan <130505226+JonathanR332@users.noreply.github.com>
2024-04-23 08:43:55 -0400

    Create Madlibs.py

---
+++ Madlibs.py
@@ -0,0 +1,20 @@
+adjective1 = input("Tell me a adjective: ")
+noun1 = input("Tell me a noun: ")
+name = input("Tell me a name: ")
+adjective2  = input("Tell me a adjective: ")
+noun2  = input("Tell me a noun: ")
+verbing  = input("Tell me a verb ending with ing: ")
+noun3 =  input("Tell me a noun: ")
+adjective3 =   input("Tell me a adjective: ")
+noun4  = input("Tell me a noun: ")
+verb2  = input("Tell me a verb: ")
+noun5  = input("Tell me a noun: ")
+adjective4  = input("Tell me a adjective: ")
+pnoun  = input("Tell me a plural noun: ")
+verb3 =   input("Tell me a verb: ")
+
+
+
+
+print("Once upon a time in a " + adjective1 + " land, there was a " + noun1 + " named " + name + ". " + name + " was known far and wide for their " + adjective2 + " " + noun2 + " skills. One day, while " + verbing + " in the " + noun2 + ", " + name + " stumbled upon a " + adjective3 + " " + noun4 + ". Curiosity got the best of " + name + " and they decided to " + verb2 + " it. To their surprise, the " + noun4 + " turned out to be a magical portal to a " + adjective4 + " world filled with " + pnoun + ". " + name + " couldn't believe their eyes! They " + verb3 + " through the portal and found themselves in the midst of a grand adventure.")
+
